a8868a668f448e7951fcd14c909e05a75cceec8a
SippingSizzurp <111225194+SippingSizzurp@users.noreply.github.com>
2024-07-07 16:41:37 -0400

    Delete 551 discord bot directory

--- 551 discord bot/commands/battlebuddy.py
+++
@@ -1,8 +0,0 @@
-import discord
-from commands.modalclasses import ClaimBBView
-
-battlebuddy = discord.SlashCommandGroup("battle-buddy", "commands for requesting battle buddies")
-
-@battlebuddy.command(name="request", description="Request a battle buddy")
-async def request(ctx: discord.ApplicationContext):
-    await ctx.send_modal(ClaimBBView(ctx, ctx.channel, title="BATTLE BUDDY REQUEST FORM"))
